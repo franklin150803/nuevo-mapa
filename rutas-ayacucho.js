6c721e75e78c374c8698bdc66fe99069b4b83d17
franklin150803 <franklincajasramirez@gmail.com>
2026-07-08 17:21:49 -0500

    Add files via upload

---
+++ rutas-ayacucho.js
@@ -0,0 +1,73 @@
+// Base de datos de transporte: Ayacucho - Huamanga (AYA_ET_001-010)
+const rutasAyacucho = {
+    "AYA_ET_001": {
+        nombre: "Empresa de Transportes 'Huamanga Express'",
+        ruta: "Terminal Terrestre - Plaza de Armas",
+        color: '#E67E22',
+        ida: [[-13.1600, -74.2250], [-13.1620, -74.2200], [-13.1630, -74.2180]],
+        vuelta: [[-13.1630, -74.2180], [-13.1620, -74.2200], [-13.1600, -74.2250]]
+    },
+    "AYA_ET_002": {
+        nombre: "Empresa de Transportes 'Ayacucho Colonial'",
+        ruta: "Urb. Los Portales - Centro",
+        color: '#2980B9',
+        ida: [[-13.1550, -74.2300], [-13.1590, -74.2250], [-13.1630, -74.2180]],
+        vuelta: [[-13.1630, -74.2180], [-13.1590, -74.2250], [-13.1550, -74.2300]]
+    },
+    "AYA_ET_003": {
+        nombre: "Empresa de Transportes 'San Juan de la Frontera'",
+        ruta: "San Juan Bautista - Plaza",
+        color: '#27AE60',
+        ida: [[-13.1500, -74.2100], [-13.1550, -74.2150], [-13.1630, -74.2180]],
+        vuelta: [[-13.1630, -74.2180], [-13.1550, -74.2150], [-13.1500, -74.2100]]
+    },
+    "AYA_ET_004": {
+        nombre: "Empresa de Transportes 'El Inti'",
+        ruta: "Urb. Andrés A. Cáceres - Mercado",
+        color: '#F1C40F',
+        ida: [[-13.1700, -74.2200], [-13.1650, -74.2200], [-13.1630, -74.2180]],
+        vuelta: [[-13.1630, -74.2180], [-13.1650, -74.2200], [-13.1700, -74.2200]]
+    },
+    "AYA_ET_005": {
+        nombre: "Empresa de Transportes 'La Libertad'",
+        ruta: "Barrio La Libertad - Av. Arenas",
+        color: '#8E44AD',
+        ida: [[-13.1600, -74.2150], [-13.1620, -74.2170], [-13.1630, -74.2180]],
+        vuelta: [[-13.1630, -74.2180], [-13.1620, -74.2170], [-13.1600, -74.2150]]
+    },
+    "AYA_ET_006": {
+        nombre: "Empresa de Transportes 'Sol de Ayacucho'",
+        ruta: "Urb. Santa Elena - Terminal",
+        color: '#C0392B',
+        ida: [[-13.1750, -74.2250], [-13.1680, -74.2250], [-13.1600, -74.2250]],
+        vuelta: [[-13.1600, -74.2250], [-13.1680, -74.2250], [-13.1750, -74.2250]]
+    },
+    "AYA_ET_007": {
+        nombre: "Empresa de Transportes 'Virgen de las Nieves'",
+        ruta: "Barrio Las Nieves - Centro",
+        color: '#1ABC9C',
+        ida: [[-13.1650, -74.2100], [-13.1640, -74.2140], [-13.1630, -74.2180]],
+        vuelta: [[-13.1630, -74.2180], [-13.1640, -74.2140], [-13.1650, -74.2100]]
+    },
+    "AYA_ET_008": {
+        nombre: "Empresa de Transportes 'Corredor Wari'",
+        ruta: "Complejo Arqueológico Wari - Huamanga",
+        color: '#34495E',
+        ida: [[-13.1000, -74.1500], [-13.1300, -74.1800], [-13.1630, -74.2180]],
+        vuelta: [[-13.1630, -74.2180], [-13.1300, -74.1800], [-13.1000, -74.1500]]
+    },
+    "AYA_ET_009": {
+        nombre: "Empresa de Transportes 'Santa Ana'",
+        ruta: "Urb. Santa Ana - Mercado",
+        color: '#D35400',
+        ida: [[-13.1580, -74.2200], [-13.1600, -74.2190], [-13.1630, -74.2180]],
+        vuelta: [[-13.1630, -74.2180], [-13.1600, -74.2190], [-13.1580, -74.2200]]
+    },
+    "AYA_ET_010": {
+        nombre: "Empresa de Transportes 'San Pedro de Ayacucho'",
+        ruta: "San Pedro - Av. Mariscal Cáceres",
+        color: '#7F8C8D',
+        ida: [[-13.1650, -74.2300], [-13.1640, -74.2250], [-13.1630, -74.2180]],
+        vuelta: [[-13.1630, -74.2180], [-13.1640, -74.2250], [-13.1650, -74.2300]]
+    }
+};
